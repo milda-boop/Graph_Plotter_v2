3e6f53ae2c07ad38353da8d8aff06d40a3b241c2
Milda Sileikaite <milda@fragum.eu>
2022-02-05 10:52:20 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Plotter_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            controller.run();
+        }
+
+       
+    }
+
+    public class Controller
+    {
+        UserInterface userinterface = new UserInterface();
+        Calculation calculation = new Calculation();
+        Representation representation = new Representation();
+        
+        public void run()
+        {
+
+        }
+    }
+
+    public class UserInterface
+    {
+
+    }
+
+    public class Calculation
+    {
+
+    }
+
+    public class Representation
+    {
+
+    }
+}
