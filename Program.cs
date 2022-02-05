dd599401506f6abeb1cbe8ced193ac4397249725
Milda Sileikaite <milda@fragum.eu>
2022-02-05 11:50:14 +0000

    created the modular classes and some userinterface methods

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Graph_Plotter_v2
        
     }
 
-    public class Controller
+    public class Controller // controls all the processes done between the three modular classes, taking in inputs and processing outputs accordingly.
     {
         UserInterface userinterface = new UserInterface();
         Calculation calculation = new Calculation();
@@ -25,13 +25,37 @@ namespace Graph_Plotter_v2
         
         public void run()
         {
-
+            userinterface.displaymenu();
         }
     }
 
     public class UserInterface
     {
+       private int userinput = 0;
 
+        public void displaymenu() // displays the menu and options to the user.
+        {
+            Console.Clear();
+            Console.WriteLine("   _____ _____            _____  _    _   _____  _      ____ _______ _______ ______ _____ ");
+            Console.WriteLine("  / ____|  __ \\     /\\   |  __ \\| |  | | |  __ \\| |    / __ \\__   __|__   __|  ____|  __ \\ ");
+            Console.WriteLine(" | |  __| |__) |   /  \\  | |__) | |__| | | |__) | |   | |  | | | |     | |  | |__  | |__) |");
+            Console.WriteLine(" | | |_ |  _  /   / /\\ \\ |  ___/|  __  | |  ___/| |   | |  | | | |     | |  |  __| |  _  / ");
+            Console.WriteLine(" | |__| | | \\ \\  / ____ \\| |    | |  | | | |    | |___| |__| | | |     | |  | |____| | \\ \\");
+            Console.WriteLine("  \\_____|_|  \\_\\/_/    \\_\\_|    |_|  |_| |_|    |______\\____/  |_|     |_|  |______|_|  \\_\\");
+            Console.WriteLine();
+            Console.WriteLine("MENU:");
+            Console.WriteLine();
+            Console.WriteLine("1  Create a graph");
+            Console.WriteLine("2  View saved graphs");
+            Console.WriteLine("3  Create a textfile");
+            Console.WriteLine("4  View saved textfiles");
+            Console.WriteLine("5  Exit");
+        } 
+        public int Getuserinput()
+        {
+            userinput = int.Parse(Console.ReadLine());
+            return userinput;
+        }
     }
 
     public class Calculation
